fc31e0bf59eed6794c502c7b019cfaffadf453c7
shantanu12 <shantanu88@tamu.edu>
2015-03-09 15:48:53 -0300

    Initial commit of app backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hlgsatracker::Application.config.secret_token = 'a7c89146949d63a3978cc7d304e52a1865c2a85ae780cc6c32c4e52e7192e34aef6774d903ea19bf5405abbd5661a0ebf5b3b41e7bc0416e47c2d7f17209e56b'
