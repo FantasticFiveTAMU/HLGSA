041b6bd058f1d3654eec9c8da85730a926c3764d
adelmoral <adelmoral13@gmail.com>
2015-04-10 19:37:46 -0300

    Added some more tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -7,6 +7,9 @@ describe EventsController do
     @event = Event.create(title: "Event", date: "2015-01-01", time: "3:00 p.m.", location: "Location", description: "Description")
 
     @event2 = Event.create(title: "Event2", date: "2015-01-01", time: "3:00 p.m.", location: "Location", description: "Description")
+
+		@events = Event.all
+
   end
 
   describe '#new' do
@@ -17,7 +20,8 @@ describe EventsController do
 
   describe '#index' do
     it 'all instances should be in Event.all' do
-      expect(@event && @event2).to be_in Event.all
+			expect(@events).not_to be_empty
+      #expect(@event && @event2).to be_in Event.all
     end
   end
 
@@ -28,8 +32,16 @@ describe EventsController do
   end
 
   describe '#create' do
-    it '' do
+    it 'should create another member and if succesful, it will have ID 3' do		
+			@event3 = Event.create(title: "Event2", date: "2015-01-01", time: "3:00 p.m.", location: "Location", description: "Description")
+			expect(@event3).to eql Event.find(3)
     end
   end
+
+	describe 'update' do
+		it 'should modify the parameters of an existing event' do
+		
+		end
+	end
 
 end
