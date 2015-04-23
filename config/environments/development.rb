1142cc2d465978d520e040d735c4ac187d62eb57
a-panda <adityaady@tamu.edu>
2015-04-23 19:51:50 -0300

    Production trial with gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,8 @@ Hlgsatracker::Application.configure do
 	address:              'smtp.gmail.com',
   port:                 587,
   domain:               'localhost:3000',
-  user_name:            saashlgsa,
-  password:             saasmail0,
+  user_name:            'saashlgsa',
+  password:             'saasmail0',
   authentication:       'plain',
   enable_starttls_auto: true  
 	}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Hlgsatracker::Application.configure do
 	address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            ENV['GMAIL_USERNAME'],
-  password:             ENV['GMAIL_PASSWORD'],
+  user_name:            'saashlgsa',
+  password:             'saasmail0',
   authentication:       'plain',
   enable_starttls_auto: true  
 	}
