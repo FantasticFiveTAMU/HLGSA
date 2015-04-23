5b26e524677529b8f0536ccf9abb7d439ed291f5
a-panda <adityaady@tamu.edu>
2015-04-23 19:15:56 -0300

    Production trial with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,13 @@ Hlgsatracker::Application.configure do
   
   
   #Mailer Details
-	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+	config.action_mailer.default_url_options = { :host => 'www.hlgsatracker.herokuapp.com' }
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.perform_deliveries = true # I can't believe I have to add this option. Does it even exist? I found it on google.
 	config.action_mailer.smtp_settings = {
 	address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'localhost:3000',
+  domain:               'www.gmail.com',
   user_name:            ENV['GMAIL_USERNAME'],
   password:             ENV['GMAIL_PASSWORD'],
   authentication:       'plain',
